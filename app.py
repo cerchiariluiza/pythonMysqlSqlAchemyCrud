9d10fe1ce7af9caf07548b7bbdb8b1789a08dc8f
cerchiariluiza <do@f>
2020-12-25 22:00:14 -0300

    nesta api, eu mudei de sqllite, para mysql, ela tem um crud, com alchemy

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restful import Api
+from resources.hotel import Hoteis, Hotel
+
+
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///banco.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/phpmyadmin'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+api = Api(app)
+
+api.add_resource(Hoteis, '/hoteis')
+api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+
+#o banco é criado antes
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
+
+#o banco colocado aqui é util para só executar quando o name(app), for executado, evitando criar sozinho
+#pip install Flask-SQLAlchemy, isso tb evita loop
+if __name__ == '__main__':
+    from sql_alchemy import banco
+    banco.init_app(app)
+    app.run(debug=True)
